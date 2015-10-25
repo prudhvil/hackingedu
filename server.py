aa1e090c1fab14881f1b5eca794a464fdafee2da
tonychenr <tonychenr@gmail.com>
2015-10-25 03:16:05 -0700

    Deployed to bluemix

--- server.py
+++ server.py
@@ -7,5 +7,7 @@ app = Flask(__name__)
 def index():
 	return app.send_static_file('index.html')
 
+port = os.getenv('VCAP_APP_PORT', '8000')
+
 if __name__ == '__main__':
-	app.run(port = 8000, host = '0.0.0.0', debug = True)
+	app.run(port = int(port), host = '0.0.0.0', debug = True)
